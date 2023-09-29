0a57c53595d0d4308fd2d4eca6df392f0ae0f879
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-09-29 12:12:00 -0300

    feat: get and post

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Comandos base:
 - config => tsconfig: "outDir": "./build",  "include": [ "src" ]
 - config => package: "scripts": { "build": "rimraf /.build && tsc", "start": "node build/index.js",
     "test": "echo \"Error: no test specified\" && exit 1", "dev": "ts-node-dev src/index.ts" }
+- npm i express
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World")
+import express, { Request, Response } from 'express'
+
+const server = express()
+server.use(express.json())
+
+server.get('/', (req: Request, res: Response) => {
+    return res.status(200).json({msg: 'DioBank API'})
+})
+
+server.post('/user', (req: Request, res: Response) => {
+    const body = req.body
+    return res.status(201).json({msg: "Usuario criado!", body})
+})
+
+const PORT = 3001
+server.listen(PORT, () => {
+    console.log("Rodando na porta", PORT)
+})
